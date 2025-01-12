6586b6418bd1c6c9443cfea8e7e6e69695b910a6
Hiroshi Kajino <hero@beyond-a.com>
2025-01-12 14:32:10 +0900

    separete Dockerfiles

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-image_name="dev-base"
-docker build --ssh default --platform linux/amd64 -t $image_name .
-docker tag $image_name:latest kanojikajino/$image_name:`date +"%Y%m%d"`
-docker push kanojikajino/dev-base:`date +"%Y%m%d"`
-
---
+++ tensorflow/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+image_name="dev-base-cuda"
+docker build --progress=plain --ssh default --platform linux/amd64 -t $image_name . #--no-cache
+docker tag $image_name:latest kanojikajino/$image_name:`date +"%Y%m%d"`
+docker push kanojikajino/$image_name:`date +"%Y%m%d"`
+
